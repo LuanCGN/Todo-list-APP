2206b5d957af6eccfd7961d34a7a91e1b26c8a00
LuanCGN <luancgn@gmail.com>
2024-04-24 12:32:23 -0300

    v4

--- TodoPixel/src/screen/TodoSceen.js
+++ TodoPixel/src/screen/TodoSceen.js
@@ -31,7 +31,19 @@ const TodoSceen = () => {
       //} 
       
       setTodoList([...todoList, {id: Date.now().toString(), title: todo}]);
-    }
+      setTodo("");
+    };
+    // Hendle Delete
+    const handleDeleteTodo = (id)=>{
+
+      const updatedTodoList = todoList.filter((todo)=> todo.id !== id)
+      
+      setTodoList(updatedTodoList);
+      
+    };
+
+
+
 
     //render todo
     const renderTodos = ({ item, index }) => {
@@ -44,7 +56,7 @@ const TodoSceen = () => {
             <Text style={{color: "#fff", fontSize: 20, fontWeight: "800", flex: 1 }}>{item.title}</Text>
 
             <IconButton icon="pencil" iconColor='#fff'/>
-            <IconButton icon="trash-can" iconColor='#fff'/>
+            <IconButton icon="trash-can" iconColor='#fff' onPress={()=>handleDeleteTodo(item.id)}/>
 
           </View>
         );
