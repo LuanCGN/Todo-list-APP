5cf11a660a95a34df488daa4f59ed271a0a29d8a
LuanCGN <luancgn@gmail.com>
2024-04-24 22:25:13 -0300

    TodoPixel-Alpha

--- TodoPixel/src/screen/TodoSceen.js
+++ TodoPixel/src/screen/TodoSceen.js
@@ -2,16 +2,6 @@ import { FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View, } from '
 import React, { useState } from 'react'
 import { Icon, IconButton } from 'react-native-paper';
 
-const dummyData = [
-  {
-    id: "01",
-    title: "Lavar o Carro",
-  },
-  {
-    id: "02",
-    title: "Ler um livro",
-  },
-];
 
 console.log(Date.now().toString());
 
@@ -19,7 +9,8 @@ const TodoSceen = () => {
 
     //init local states
     const[todo, setTodo] = useState("");
-    const[todoList, setTodoList] = useState([])
+    const[todoList, setTodoList] = useState([]);
+    const[editedTodo, setEditedTodo] = useState(null);
     //Handle add todo
     
     const handleAddTodo = ()=>{
@@ -29,6 +20,9 @@ const TodoSceen = () => {
       //  id:
       //  title:
       //} 
+      if(todo === ""){
+        return;
+      }
       
       setTodoList([...todoList, {id: Date.now().toString(), title: todo}]);
       setTodo("");
@@ -42,20 +36,61 @@ const TodoSceen = () => {
       
     };
 
+    //Handle Edit todo
+
+    const handleEditTodo = (todo)=>{
+
+      setEditedTodo(todo);
+      setTodo(todo.title);
+     
+          
+    };
+
+    // Handle Update
+
+    const handleUpdateTodo = ()=>{
+
+      const updatedTodos = todoList.map((item)=>{
+
+        if(item.id === editedTodo.id){
+          return {...item, title: todo}
+        }
+
+        return item
+
+        
+
+      });
+      setTodoList(updatedTodos);
+      setEditedTodo(null);
+      setTodo("");
+    };
+
 
 
 
     //render todo
     const renderTodos = ({ item, index }) => {
         return (
-          <View style={{backgroundColor:"#1e90ff" , borderRadius: 6, paddingHorizontal:6, paddingVertical:12, marginBottom: 12, 
-          flexDirection: "row", alignItems:"center"}}>
+          <View style={{
+            backgroundColor:"#1e90ff" , 
+            borderRadius: 6, 
+            paddingHorizontal:6, 
+            paddingVertical:12, 
+            marginBottom: 12, 
+            flexDirection: "row", 
+            alignItems:"center",
+            shadowColor: "#000",
+            shadowOffset:{width: 0, height: 2},
+            shadowOpacity: 0.8,
+            
+            }}>
 
             
 
             <Text style={{color: "#fff", fontSize: 20, fontWeight: "800", flex: 1 }}>{item.title}</Text>
 
-            <IconButton icon="pencil" iconColor='#fff'/>
+            <IconButton icon="pencil" iconColor='#fff' onPress={()=>handleEditTodo(item)}/>
             <IconButton icon="trash-can" iconColor='#fff' onPress={()=>handleDeleteTodo(item.id)}/>
 
           </View>
@@ -82,12 +117,35 @@ const TodoSceen = () => {
       
       />
 
-      <TouchableOpacity style={{backgroundColor:"#000", borderRadius: 6, paddingVertical: 10, marginVertical: 34, alignItems: "center",}}
-      onPress={()=> handleAddTodo()}
-      >
+      {
+        editedTodo ? 
+        <TouchableOpacity style={{
+          backgroundColor:"#000", 
+          borderRadius: 6, 
+          paddingVertical: 10, 
+          marginVertical: 34, 
+          alignItems: "center",}}
+          onPress={()=> handleUpdateTodo()}
+        >
+          
+          <Text style={{color: "#fff" , fontWeight: "bold", fontSize: 20}}>Salvar</Text>
+        </TouchableOpacity> :
+        <TouchableOpacity style={{
+          backgroundColor:"#000", 
+          borderRadius: 6, 
+          paddingVertical: 10, 
+          marginVertical: 34, 
+          alignItems: "center",}}
+          onPress={()=> handleAddTodo()}
+        >
+          
+          <Text style={{color: "#fff" , fontWeight: "bold", fontSize: 20}}>Salvar</Text>
+        </TouchableOpacity>
         
-        <Text style={{color: "#fff" , fontWeight: "bold", fontSize: 20}}>Adicione</Text>
-      </TouchableOpacity>
+        
+
+      }
+      
 
     {/*Render todo list*/}
 
