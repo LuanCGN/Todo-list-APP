a2772f1b956c1a263aeaff0b38651a31cb5abe43
LuanCGN <luancgn@gmail.com>
2024-05-20 10:12:57 -0300

    TodoPixel Version - 1.00

--- TodoPixel/src/components/Fallback.js
+++ TodoPixel/src/components/Fallback.js
@@ -3,41 +3,33 @@ import React from 'react'
 
 const Fallback = () => {
   return (
-    
-
-    
-    <View style={{
-      justifyContent: 'center',
-      alignItems: 'center',
-
-      }}>
-
-        <Image source={require("../../assets/todo.png")} 
-        style={{
-            height: 290, 
-            width: 290,
-            justifyContent: 'center',
-            alignItems: 'center',
-            top: '5%',
-
-            }}/>
-
-
-
-      <Text style = {{
-        color: "#fff" ,
-        fontSize: 16,
-        top: '20%', 
-        fontWeight: '200',
-        backgroundColor:"#000", 
-        borderRadius: 70,
-        marginHorizontal: 70,
-
-      }}>---adicione suas tarefas---</Text>
+    <View style={styles.container}>
+        <Image source={require("../../assets/todo.png")} style={styles.image} />
+        <Text style={styles.text}>adicione suas tarefas</Text>
     </View>
   )
 }
 
 export default Fallback
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 18,
+  },
+  image: {
+    height: 290,
+    width: 290,
+  },
+  text: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: '150',
+    backgroundColor: "#000",
+    borderRadius: 70,
+    marginTop: 50,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+  }
+});
--- TodoPixel/src/screen/TodoSceen.js
+++ TodoPixel/src/screen/TodoSceen.js
@@ -1,29 +1,23 @@
-import { FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { IconButton } from 'react-native-paper';
 import Fallback from '../components/Fallback';
-//Imports: Aqui estamos importando os componentes necessários do React Native, como FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View, e também do AsyncStorage para armazenamento local, o IconButton do react-native-paper e um componente de fallback personalizado.
 
-const TodoSceen = () => {
-    // Define o componente funcional TodoSceen.
+const TodoScreen = () => {
     const [todo, setTodo] = useState('');
     const [todoList, setTodoList] = useState([]);
     const [editedTodo, setEditedTodo] = useState(null);
-    // useState: Aqui são definidos os estados do componente. todo armazena a entrada atual do usuário, todoList armazena a lista de tarefas, e editedTodo armazena a tarefa que está sendo editada.
 
     useEffect(() => {
-        // Carrega a lista de tarefas armazenadas ao iniciar o aplicativo. Este useEffect executa apenas uma vez, quando o componente é montado.
         loadTodoList();
     }, []);
 
     useEffect(() => {
-        // Salva a lista de tarefas sempre que ela é modificada. Este useEffect é acionado toda vez que todoList é alterado.
         saveTodoList();
     }, [todoList]);
 
     const loadTodoList = async () => {
-        // Função para carregar as tarefas armazenadas
         try {
             const storedTodoList = await AsyncStorage.getItem('todoList');
             if (storedTodoList !== null) {
@@ -35,17 +29,14 @@ const TodoSceen = () => {
     };
 
     const saveTodoList = async () => {
-        // Função para salvar a lista de tarefas
         try {
             await AsyncStorage.setItem('todoList', JSON.stringify(todoList));
         } catch (error) {
             console.error('Erro ao salvar a lista de tarefas:', error);
         }
-        //Funções de carregamento e salvamento: loadTodoList carrega as tarefas armazenadas e saveTodoList salva a lista de tarefas no AsyncStorage.
     };
 
     const handleAddTodo = () => {
-        // Função para adicionar uma nova tarefa à lista
         if (todo === '') {
             return;
         }
@@ -54,19 +45,16 @@ const TodoSceen = () => {
     };
 
     const handleDeleteTodo = (id) => {
-        // Função para excluir uma tarefa da lista
         const updatedTodoList = todoList.filter((todo) => todo.id !== id);
         setTodoList(updatedTodoList);
     };
 
     const handleEditTodo = (todo) => {
-        // Função para editar uma tarefa da lista
         setEditedTodo(todo);
         setTodo(todo.title);
     };
 
     const handleUpdateTodo = () => {
-        // Função para atualizar uma tarefa da lista
         const updatedTodos = todoList.map((item) => {
             if (item.id === editedTodo.id) {
                 return { ...item, title: todo };
@@ -76,33 +64,13 @@ const TodoSceen = () => {
         setTodoList(updatedTodos);
         setEditedTodo(null);
         setTodo('');
-        //Funções de manipulação de tarefas: handleAddTodo adiciona uma nova tarefa, handleDeleteTodo exclui uma tarefa, handleEditTodo permite editar uma tarefa e handleUpdateTodo atualiza uma tarefa editada.
     };
 
-    const renderTodos = ({ item, index }) => {
-        // Função para renderizar cada item da lista de tarefas
+    const renderTodos = ({ item }) => {
         return (
-            <View style={{ 
-                backgroundColor: '#40e0d0', 
-                borderRadius: 6, 
-                paddingHorizontal: 6, 
-                paddingVertical: 5, 
-                marginBottom: 12, 
-                flexDirection: 'row', 
-                alignItems: 'center', 
-                shadowColor: '#000', 
-                shadowOffset: 
-                { width: 0, 
-                height: 2 
-                }, 
-                shadowOpacity: 0.8 
-                }}>
-                <Text style={{ 
-                    color: '#000000', 
-                    fontSize: 20, 
-                    fontWeight: '400', 
-                    flex: 1 }}>
-                        {item.title}
+            <View style={styles.todoItem}>
+                <Text style={styles.todoText}>
+                    {item.title}
                 </Text>
                 <IconButton icon="pencil" iconColor="#fff" onPress={() => handleEditTodo(item)} />
                 <IconButton icon="trash-can" iconColor="#fff" onPress={() => handleDeleteTodo(item.id)} />
@@ -111,72 +79,96 @@ const TodoSceen = () => {
     };
 
     return (
-        // O restante do código renderiza a interface do usuário
-        <View style={{ 
-            marginHorizontal: 16, 
-            paddingTop: 40 
-            }}>
-            <View style={{ 
-                flexDirection: 'row', 
-                marginBottom: 20, 
-                justifyContent: 'center', 
-                alignItems: 'center' 
-            }}>
-                <Text style={{ fontSize: 30, 
-                    fontWeight: 'bold' }}>Todo</Text>
-                <Text style={{ 
-                    fontSize: 30, 
-                    fontWeight: 'bold', 
-                    color: '#40e0d0' 
-                 }}>Pixel</Text>
+        <ScrollView contentContainerStyle={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.title}>Todo</Text>
+                <Text style={styles.titleAccent}>Pixel</Text>
             </View>
-            <TextInput style={{ 
-                borderWidth: 3, 
-                borderColor: '#40e0d0', 
-                borderRadius: 10, 
-                paddingVertical: 8, 
-                paddingHorizontal: 16 
-                }} 
-                placeholder="Adicione uma Tarefa" value={todo} onChangeText={(userText) => setTodo(userText)} />
+            <TextInput
+                style={styles.input}
+                placeholder="Adicione uma Tarefa"
+                value={todo}
+                onChangeText={(userText) => setTodo(userText)}
+            />
             {editedTodo ? (
-                <TouchableOpacity
-                    style={{ 
-                    backgroundColor: '#000', 
-                    borderRadius: 6, 
-                    paddingVertical: 10, 
-                    marginVertical: 34, 
-                    alignItems: 'center' 
-                    }}
-                    onPress={() => handleUpdateTodo()}>
-                    <Text style={{ color: '#fff', 
-                    fontWeight: 'bold', 
-                    fontSize: 20 
-                    }}>Salvar</Text>
+                <TouchableOpacity style={styles.button} onPress={() => handleUpdateTodo()}>
+                    <Text style={styles.buttonText}>Salvar</Text>
                 </TouchableOpacity>
             ) : (
-                <TouchableOpacity
-                    style={{ 
-                        backgroundColor: '#000', 
-                        borderRadius: 6, 
-                        paddingVertical: 10, 
-                        marginVertical: 34, 
-                        alignItems: 'center' 
-                    }}
-                    onPress={() => handleAddTodo()}>
-                    <Text style={{ 
-                        color: '#fff', 
-                        fontWeight: 'bold', 
-                        fontSize: 20 }}>Salvar</Text>
+                <TouchableOpacity style={styles.button} onPress={() => handleAddTodo()}>
+                    <Text style={styles.buttonText}>Salvar</Text>
                 </TouchableOpacity>
             )}
-            <FlatList data={todoList} renderItem={renderTodos} keyExtractor={(item) => item.id} />
+            {todoList.map((item) => (
+                <View key={item.id} style={styles.todoItem}>
+                    <Text style={styles.todoText}>{item.title}</Text>
+                    <IconButton icon="pencil" iconColor="#fff" onPress={() => handleEditTodo(item)} />
+                    <IconButton icon="trash-can" iconColor="#fff" onPress={() => handleDeleteTodo(item.id)} />
+                </View>
+            ))}
             {todoList.length <= 0 && <Fallback />}
-            
-        </View>
-        //Retorno do componente: Renderiza a interface do usuário, incluindo o título, entrada de texto para adicionar tarefas, botões para salvar, editar e excluir tarefas, e a lista de tarefas renderizada usando FlatList. Também inclui um componente de fallback se a lista estiver vazia.
+        </ScrollView>
     );
 };
 
-export default TodoSceen;
+const styles = StyleSheet.create({
+    container: {
+        paddingHorizontal: 16,
+        paddingTop: 40,
+        paddingBottom: 20, // Adiciona espaço na parte inferior para melhorar a rolagem
+    },
+    header: {
+        flexDirection: 'row',
+        marginBottom: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+    titleAccent: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#40e0d0',
+    },
+    input: {
+        borderWidth: 3,
+        borderColor: '#40e0d0',
+        borderRadius: 10,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    },
+    button: {
+        backgroundColor: '#000',
+        borderRadius: 6,
+        paddingVertical: 10,
+        marginVertical: 34,
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 20,
+    },
+    todoItem: {
+        backgroundColor: '#40e0d0',
+        borderRadius: 6,
+        paddingHorizontal: 6,
+        paddingVertical: 5,
+        marginBottom: 12,
+        flexDirection: 'row',
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.8,
+    },
+    todoText: {
+        color: '#000000',
+        fontSize: 20,
+        fontWeight: '400',
+        flex: 1,
+    },
+});
 
-const styles = StyleSheet.create({});
+export default TodoScreen;
